d1e3d327648f6a65700ca886229277b7174a6a44
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-21 10:37:51 -0700

    Database Access is Built

--- server/api/users/users-model.js
+++ server/api/users/users-model.js
@@ -1,18 +1,32 @@
 
 
 function find() {
+    return db('user')
 }
 
 function findById(id) {
+    first()
+    return db('user').where({ id }).first()
 }
 
 function add(user) {
+    db('user').insert(user)
+        .then(ids=> {
+            return findById(id)
+        })
 }
 
-function update(changes, id) {
-}
+function update(user, id) {
+    return db('user')
+    .where('id', Number(id))
+    .update(user)
+    }
+
 
 function remove(id) {
+    return db('user')
+    .where('id', Number(id))
+    .del();
 }
 
 module.exports = {
